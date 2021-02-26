84ce88aa74ee37b76c2d41f2e7d2067585d4b6c7
Priscila Falcão <falcao.priscila@hotmail.com>
2021-02-26 18:44:36 -0300

    Project creation, first components and base css added.

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/64566788?s=400&u=083875e7f81e197a3b64c2bf99ead55ce2d2b8b4&v=4" alt="Avatar Provisório"/>
+            <div>
+                <strong>Priscila Falcão</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}

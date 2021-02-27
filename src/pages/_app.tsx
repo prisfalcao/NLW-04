379eadfdb2b250740aaaf81b26c20b7b04f02fc2
Priscila Falcão <falcao.priscila@hotmail.com>
2021-02-27 14:50:09 -0300

    Added level, experience, and level up functions.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return ( 
-  <ChallengesProvider> 
-  <Component {...pageProps} />
+  <ChallengesProvider>
+      <Component {...pageProps} />
   </ChallengesProvider> 
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import { ChallengeBox } from '../components/ChallengeBox';
 import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 
 
@@ -18,7 +19,7 @@ export default function Home() {
       </Head>
       
       <ExperienceBar />
-
+      <CountdownProvider>
       <section>
         <div>
           <Profile />
@@ -29,6 +30,7 @@ export default function Home() {
           <ChallengeBox />
         </div>
       </section>
+      </CountdownProvider>
     </div>
   )
 }

60662f08c1f37b3459fffa4620669ff8fe71e10a
Priscila Falcão <falcao.priscila@hotmail.com>
2021-02-27 18:32:13 -0300

    Added storage using Cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
@@ -7,13 +8,24 @@ import { Profile } from '../components/Profile';
 import { ChallengeBox } from '../components/ChallengeBox';
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
 
+export default function Home(props: HomeProps) {
 
-export default function Home() {
   return (
+    <ChallengesProvider 
+    level={props.level} 
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}>
     <div className={styles.container}>
+
       <Head>
         <title>Início | move.it</title>
       </Head>
@@ -32,5 +44,18 @@ export default function Home() {
       </section>
       </CountdownProvider>
     </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
